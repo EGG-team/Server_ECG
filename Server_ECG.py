a3a826ee917f0f521526afbb4fa857c4ba9221f1
Alex <Ksh_sa@mail.ru>
2018-04-13 20:13:58 +0300

    index

--- Server_ECG.py
+++ Server_ECG.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/simple_chart')
+def chart():
+    values = [0,1,2,3,4,5,6,7,8,9]
+    return render_template('chart.html', values = values)
+
 
 if __name__ == '__main__':
     app.run()
